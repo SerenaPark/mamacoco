e890265bc9460de3c5fd6695199e8d6952f9e1c6
Serena <sooyoung.data@gmail.com>
2024-03-02 17:35:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.05)
      my_bar.progress(percent_complete + 1)
- st.balloons()
+
+st.balloons()
 
 # st.title('this is title')
 # st.header('this is header')
